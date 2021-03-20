1536bcf29e1fa9587a2b31294e6965240fc09506
mmorrisonlk <mmorrisonlk@gmail.com>
2021-03-19 21:45:56 -0500

    Laid out the basic structure using Materialize

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 // fetch("api.openweathermap.org/data/2.5/forecast?q="{Dallas}"&appid="{8d2d1058eec9accebef78f8687d8ea63})
-const APIkey = "8d2d1058eec9accebef78f8687d8ea63";
-fetch("api.openweathermap.org/data/2.5/forecast?id=524901&APPID=" + APIkey)
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+// const APIkey = "8d2d1058eec9accebef78f8687d8ea63";
+// fetch("https://api.openweathermap.org/data/2.5/forecast?zip=75206,us&appid=8d2d1058eec9accebef78f8687d8ea63")
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.log(error));
